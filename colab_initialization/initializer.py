b90290708d33b143ed027bfbec929040bff32c8b
thowley0824 <91752974+thowley0824@users.noreply.github.com>
2024-01-16 05:24:35 -0500

    Create initializer.py

---
+++ colab_initialization/initializer.py
@@ -0,0 +1,28 @@
+import os
+from google.colab import drive
+
+def initialize_project(capstone_project_path = '/content/drive/MyDrive/capstone/'):
+
+    drive.mount('/content/drive', force_remount=True)
+
+    if not os.path.exists(capstone_project_path):
+
+        os.mkdir(capstone_project_path)
+        print(f"""Top level project directory 'capstone' has been created on Google drive.
+        This directory will be used to store and access data and code for use in this project.
+        It is accessible at the following path via Colab: {capstone_project_path}
+        """)
+    else:
+        print(f"""Top level project directory 'capstone' already exists on Google drive.
+        This directory will be used to store and access data and code for use in this project.
+        It is accessible at the following path via Colab: {capstone_project_path}
+        """)
+
+    os.chdir(capstone_project_path)
+    print(f"""Current working directory is now the top-level capstone project directory. 
+    It is located in the mounted Google Drive and accessible at the following 
+    path via Colab: {capstone_project_path}
+    """)
+
+    print(f"""Project successfully initialized.""")
+    return None
